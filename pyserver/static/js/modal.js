ef6625931706a9346c6bef0a2a5f2dd6fcfa7865
Sourav <souravkumarraj@decorpot.com>
2021-02-08 15:13:21 +0530

    login ui and wd modal ui

---
+++ pyserver/static/js/modal.js
@@ -0,0 +1,446 @@
+// state variable to contain parsed JSON data
+var state = {};
+
+// variables for overlaying comment canvas( '.overlay-canvas-container' )
+var overlayCanvasContainers = document.querySelectorAll(".overlay-canvas-container");
+var w = document.querySelector(`#wd-0 canvas`).width;
+var h = document.querySelector(`#wd-0 canvas`).height;
+var overlayCanvases = [];
+overlayCanvasContainers.forEach((canvas, id) => {
+    let canv = new fabric.Canvas(`c#${id}`, {
+        backgroundColor: "rgba(0, 0, 0, 0)",
+        width: w,
+        height: h,
+    });
+    overlayCanvases.push(canv);
+});
+
+var canvasJSONs = [];
+
+var currentPageNumber = 1;
+const viewType = ["room_top_view", "top_view", "front_view", "internal_view"];
+
+var openNewJSON = false;
+
+
+const readJSO = function (input) {
+    try {
+
+        $("#loader").toggle();
+        $(".main").css({ opacity: 0.5 });
+        if (input) {
+            fetch(input).then(response => response.json()).then(res => {
+                openNewJSON = true;
+
+                let parsedData;
+                console.log(JSON.stringify(res));
+                // python server
+                const url = "http://localhost:4000/json";
+                const othePram = {
+                    headers: {
+                        "content-type": "application/json; charset=UTF-8",
+                    },
+
+                    body: JSON.stringify(JSON.stringify(res)),
+                    method: "POST",
+                };
+
+                fetch(url, othePram)
+                    .then((response) => response.json())
+                    .then((data) => {
+                        parsedData = data;
+                        // console.log(parsedData);
+
+                        /* PARSE JSON data */
+                        parseJSO(parsedData);
+
+                        /*     RENDER PART     */
+                        renderAl();
+
+                        // remove loading bar & restore the background
+                        $("#loader").toggle();
+                        $(".main").css({ opacity: 1 });
+                    })
+                    .catch((error) => {
+                        // remove loading bar & restore the background
+                        $("#loader").toggle();
+                        $(".main").css({ opacity: 1 });
+                        console.log(error);
+                        alert("There's problem with data. Please try again.");
+                    });
+                //
+                //};
+                //   reader.readAsText(input.files[0]);
+            })
+        } else {
+            // remove loading bar & restore the background
+            $("#loader").toggle();
+            $(".main").css({ opacity: 1 });
+            // alert user
+            alert("Please import json file, not other file.");
+        }
+    } catch (e) {
+        // remove loading bar & restore the background
+        $("#loader").toggle();
+        $(".main").css({ opacity: 1 });
+        console.log(e);
+    }
+};
+
+const parseJSO = (parsedData) => {
+    if (!parsedData) return;
+    try {
+        // reinitialize state variable
+        state = {};
+
+        // get project_ & org_details
+        state.projectInfo = getProjectInfo(parsedData);
+
+        // get floor plan & names of rooms
+        const info = getFloorPlan(parsedData);
+        state.roomViews = [info[0]]; // push floorPlanView
+        state.roomNames = info[1];
+
+        // get material thumbnails & rooms part(JSON)
+        let res = getRoomDetails(parsedData, state.roomNames);
+        state.matThumbnails = res.matThumbnails;
+        state.rooms = res.rooms;
+
+        // parse 'state.rooms' to get room views
+        let subViews = getRoomObjects(state.rooms);
+        state.roomViews = [...state.roomViews, ...subViews[0]];
+
+        // get py dimens
+        state.dimens = [];
+        state.dimens.push(info[2]); // ground floror plan (0)
+        state.dimens = [...state.dimens, ...subViews[1]];
+
+        // get py viewBoxInfo
+        state.viewBoxInfo = [];
+        state.viewBoxInfo.push(info[3]);
+        state.viewBoxInfo = [...state.viewBoxInfo, ...subViews[2]];
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+const renderAl = () => {
+    // if no data on state variable
+    if (Object.keys(state).length === 0) return;
+
+    // empty existing WDs || reinitialize
+    document.querySelector(".main").innerHTML = ``;
+
+    // render project and org info ( footer table )
+    renderProjectInfo(state.projectInfo, state.roomViews.length);
+
+    // calibrate canvas
+    calibrateCanvases(state.viewBoxInfo);
+
+    // reinitialize the variables
+
+    // also for overlayCanvases
+    overlayCanvasContainers = document.querySelectorAll(".overlay-canvas-container");
+    w = document.querySelector(`#wd-0 canvas`).width;
+    h = document.querySelector(`#wd-0 canvas`).height;
+    overlayCanvases = [];
+    overlayCanvasContainers.forEach((canvas, id) => {
+        let canv = new fabric.Canvas(`c#${id}`, {
+            backgroundColor: "rgba(0, 0, 0, 0)",
+            width: w,
+            height: h,
+        });
+
+        if (canvasJSONs.length !== 0) {
+            // restore canvas objects state
+            if (Object.keys(canvasJSONs[id]) !== 0) {
+                let json = canvasJSONs[id];
+                //
+                function CallBack() {
+                    canv.renderAl();
+                    canv.calcOffset();
+                }
+                canv.loadFromJSON(json, CallBack, function (o, object) {
+                    canv.setActiveObject(object);
+                });
+            }
+        }
+
+        overlayCanvases.push(canv);
+    });
+
+    // render views
+    state.roomViews.forEach((view, id) => {
+        // floorPlanView
+        if (id === 0) {
+            renderFloorPlan(view);
+        }
+        // other views like RoomSubView, RenderView ...
+        else {
+            renderView(view, id);
+            if (viewType.includes(view.getName())) {
+            }
+            if (view.type === "ImageView") {
+                // render material thumbnails
+                renderMaterialThumbnails(state.matThumbnails, id);
+            }
+        }
+    });
+    if (openNewJSON) {
+        // draw py dimens
+        state.dimens.forEach((dimensions, id) => {
+            const viewName = state.roomViews[id].getName();
+            if (viewName != "table_view") {
+                renderPyDimensions(dimensions, id);
+            }
+        });
+    }
+};
+
+// print statement
+$("#print").on("click", function (e) {
+    print();
+});
+
+function print() {
+    $("#loader").toggle();
+    $(".main").css({ opacity: 0.5 });
+
+    let totalPagesNumber = state.roomViews ? state.roomViews.length : 1;
+    const ele = document.querySelector(`#wd-0`);
+    ele.scrollIntoView({ block: "start" });
+
+    let date = new Date();
+    let datestr = date.toISOString();
+    datestr = datestr.replace(/[^0-9]/g, "");
+    const filename = `Drawings_${datestr}.pdf`;
+
+    let doc = new jsPDF("l", "px", "a3");
+    if (totalPagesNumber === 1) {
+        const ele = document.getElementById(`wd-0`);
+        console.log(ele.clientWidth, ele.clientHeight);
+        const opt = {
+            margin: 30,
+            filename: filename,
+            image: { type: "jpeg", quality: 1 },
+            html2canvas: { scale: 3 },
+            jsPDF: { unit: "mm", format: "a3", orientation: "l" },
+        };
+        html2pdf().set(opt).from(ele).save();
+
+        $("#loader").toggle();
+        $(".main").css({ opacity: 1 });
+    } else {
+        // Todo: create multiple pages
+        const opt = {
+            image: { type: "jpeg", quality: 5 },
+            html2canvas: {
+                scale: 5,
+                dpi: 400,
+                letterRendering: true,
+                width: 1444, // 1145
+                height: 858, // 815
+            },
+        };
+        let id = (testid = 0);
+        let myTimer = setInterval(function () {
+            if (id === totalPagesNumber) {
+                myStopFunc();
+            }
+            if (id < totalPagesNumber) {
+                if (id !== testid || id == 0) {
+                    testid = id;
+                    if (id > 0) {
+                        doc.addPage();
+                    }
+                    const ele = document.querySelector(`#wd-${id}`);
+
+                    html2pdf()
+                        .set(opt)
+                        .from(ele)
+                        .outputImg("dataurlstring")
+                        .then((result) => {
+                            doc.addImage(
+                                result,
+                                "jpeg",
+                                0,
+                                0,
+                                doc.internal.pageSize.width,
+                                doc.internal.pageSize.height
+                            );
+                            id++;
+                        });
+                }
+            }
+        }, 600);
+        function myStopFunc() {
+            clearInterval(myTimer);
+
+            $("#loader").toggle();
+            $(".main").css({ opacity: 1 });
+
+            doc.save(filename);
+            delete doc;
+            alert("Downloading PDF completed!!!");
+        }
+    }
+}
+
+$(window).resize(resizeCanvas);
+
+function resizeCanvas() {
+    let len = 0;
+    if (!state.roomViews) {
+        len = 1;
+    } else {
+        len = state.roomViews.length;
+    }
+    for (let i = 0; i < len; i++) {
+        const outerCanvasContainer = $(".canvas-container")[i];
+
+        const canvas = overlayCanvases[i];
+
+        const ratio = canvas.getWidth() / canvas.getHeight();
+        const containerWidth = outerCanvasContainer.clientWidth;
+        const containerHeight = outerCanvasContainer.clientHeight;
+
+        const scale = containerWidth / canvas.getWidth();
+        const zoom = canvas.getZoom() * scale;
+        // canvas.setDimensions({ width: containerWidth, height: containerWidth / ratio });
+        // canvas.setViewportTransform([zoom, 0, 0, zoom, 0, 0]);
+        canvas.setDimensions({ width: containerWidth, height: containerHeight });
+        canvas.setViewportTransform([zoom, 0, 0, 1, 0, 0]);
+    }
+}
+
+
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+var myArray = ["stone", "paper", "scissor"];
+
+// When the user clicks the button, open the modal 
+window.onload = function () {
+
+
+
+    // fetch('https://naraci-test.s3.ap-south-1.amazonaws.com/5ea7fd5fc5c27f1e749fc39c/v1/New+Version+Test+Json+Kitchen+copy.json').then(response=>response.json()).then(res=> {
+    //     console.log(res)
+    //     readJSO(res);
+    // })
+    // return;
+
+
+}
+// downloadFile = function(id){
+//     console.log(id);
+//     // document.getElementById(id).previousElementSibling
+// }
+var userPrj = [], version = [];
+btn.onclick = function () {
+    modal.style.display = "block";
+    userPrj = [], version = [];
+    $("#loader").toggle();
+    $(".modal").css({ opacity: 0.5 });
+    const userId = localStorage.getItem("userId");
+    let userProject = [];
+    console.log(document.getElementById('modal'), localStorage.getItem("token"), userId);
+    fetch('http://localhost:5000/api/project/wdProject', {
+        method: 'GET',
+        headers: {
+            'Content-type': 'application/json', // The type of data you're sending
+            'authorization': localStorage.getItem("token")
+        }
+    }).then(function (response) {
+        console.log(response)
+        if (response.ok) {
+            return response.json();
+        }
+        return Promise.reject(response);
+    }).then(function (data) {
+        console.log(data);
+        $("#loader").toggle();
+        $(".modal").css({ opacity: 1 });
+        let project = [];
+        data.forEach(el => {
+            console.log(data, 'data');
+            if (el.workingDrawing) {
+                let obj = {
+                    "project_no": el.project_no,
+                    "version": el.workingDrawing
+                }
+                userProject.push(el.project_no);
+                // version.push(el.workingDrawing);
+                version.push(obj);
+            }
+        })
+        if (userProject.length === 0 && version.length === 0) {
+            document.getElementById('modal').innerHTML += '<br>' + 'Version not found';
+            return;
+        }
+        userPrj = [...userProject]
+        userPrj.forEach(function (el) {
+            document.getElementById('modal').innerHTML += '<br>' + el;
+        })
+        console.log(version, 'version');
+        version.forEach(function (el) {
+            console.log(el);
+            document.getElementById('versionNumber').innerHTML += '<br>'
+            el.version.forEach(function (ele) {
+                ele.wdFile = 'https://naraci-test.s3.ap-south-1.amazonaws.com/5ea7fd5fc5c27f1e749fc39c/v1/New+Version+Test+Json+Kitchen+copy.json'
+
+                document.getElementById('versionNumber').innerHTML += ele.version + ' ' + `<i id=${ele.version}-${el.project_no} class="fa fa-download" style="margin-right:2%;"></i>` + '  '
+            })
+            // document.getElementById('versionNumber').innerHTML +=  '<br>'
+        })
+        console.log(userPrj.length, version.length);
+
+    }).catch(function (error) {
+        console.warn('Something went wrong.', error);
+    });
+
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+    clear()
+
+}
+function clear() {
+    document.getElementById("modal").innerHTML = "";
+    document.getElementById("versionNumber").innerHTML = "";
+}
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+    clear()
+}
+
+
+
+document.getElementById("versionNumber").onclick = function (e) {
+    console.log(e.target.id.split('-'), 'versionNumber', version);
+    prjAndVersionArr = e.target.id.split('-');
+    version.forEach(function (el) {
+        if (el.project_no === prjAndVersionArr[1]) {
+            console.log(el);
+            el.version.forEach(function (ele) {
+                console.log(ele.version, prjAndVersionArr[0]);
+                if (ele.version === prjAndVersionArr[0]) {
+                    console.log(ele, 'wdFile');
+                    readJSO(ele.wdFile)
+                }
+            })
+        }
+    })
+    modal.style.display = "none";
+
+    console.log(document.getElementById(e.target.id).parentNode)
+}

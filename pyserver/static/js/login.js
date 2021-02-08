7a277d3808881b82606f2a0b4be5ee8b145ad186
Sourav <souravkumarraj@decorpot.com>
2021-02-08 15:33:02 +0530

    removing unused comment

--- pyserver/static/js/login.js
+++ pyserver/static/js/login.js
@@ -1,19 +1,15 @@
 const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-form-submit");
-const loginErrorMsg = document.getElementById("login-error-msg");
-​
-​
+
 loginButton.addEventListener("click", (e) => {
     e.preventDefault();
     const username = loginForm.username.value;
     const password = loginForm.password.value;
+    console.log(username, 'username', password, 'password');
     let data = {
         "email": loginForm.username.value,
         "password": loginForm.password.value
     }
-​
-    console.log(username, 'username', password, 'password');
-    
     fetch('http://15.207.19.54:8080/api/user/login', {
         method: 'POST',
         body: JSON.stringify(data), // The data
@@ -27,17 +23,14 @@ loginButton.addEventListener("click", (e) => {
         }
         return Promise.reject(response);
     }).then(function (data) {
-        location.href= "static/wdPdf.html"
+        localStorage.setItem('token', data.token)
+        userId = data.user['_id']
+        localStorage.setItem('userId', data.user['_id'])
+        location.pathname = "static/wdPdf.html"
         console.log(data);
     }).catch(function (error) {
         console.warn('Something went wrong.', error);
     });
 })
-function loginFun() {
-    console.log('lokjiu');
-}
-// document.getElementById("login-form-submit").onclick=function (e) {
-//     location.href="https://www.youtube.com/"
-//     console.log('llllll');
-​
-// };
+
+// module.exports = {userId} ;
